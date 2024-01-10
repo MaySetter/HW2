1d9fa5beb53ff1d18c52725ce97eca7bf415cbf2
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-10 20:42:30 +0200

    Update Track.java

--- src/Race/Track.java
+++ src/Race/Track.java
@@ -1,8 +1,10 @@
 package Race;
 
 public class Track {
-    private int finishedRacers = 0;
-
+    private int finishedRacers;
+    Trrack(){
+      this.finishedRacers=0;
+    }
     public int getFinishedRacers(){ return finishedRacers;}
     public void setFinishedRacers(){ finishedRacers++;}
 }

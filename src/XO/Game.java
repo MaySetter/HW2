7325c33bb9f89ae830bf1cb7bf0c8ab3a2687682
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-13 18:56:00 +0200

    Update Game.java

--- src/XO/Game.java
+++ src/XO/Game.java
@@ -1,28 +1,49 @@
 package XO;
-
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Random;
 public abstract class Game {
-
-    public char[][] gameBoard = new char[5][5];
-
-    public enum SIGNS {X,O}
-
-    public Game(){}
-
-    public void printBoard(){
-        for (int i=0; i<gameBoard.length; i++){
-            System.out.println("+---+---+---+---+---+");
-            for (int j=0; j<gameBoard[i].length; j++){
-                System.out.print("| " + gameBoard[i][j] + " | ");
+    private char[][] gameBoard = new char[5][5];
+    private char turn;
+    public Game(){
+    	for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard[i].length; j++) {
+                gameBoard[i][j] = ' ';                 
             }
-        }
-        System.out.println("+---+---+---+---+---+");
-    };
-
-    public abstract int getTurn();
-
+    	}
+    	Random random=new Random();
+    	int temp=random.nextInt(2);
+    	if(temp==1)
+    		this.turn='X';
+    	else this.turn='O';
+    	System.out.println("First turn : "+this.turn);
+    	printBoard();
+    }
+    public char[][] getGameBoard() {
+	    return this.gameBoard;
+	}
+    public void setGameBoard(Coordinates a,char symbol) {
+	    this.gameBoard[a.getRow()][a.getCol()]=symbol;
+	}
+    public void setTurn() {
+	   if(turn=='X')
+		   turn='O';
+	   else
+		   turn='X';
+	}
+    public char getTurn() {
+		return turn; 	
+    }
+    public synchronized void printBoard(){
+    	 for (int i=0; i<gameBoard.length; i++){
+             System.out.println("+----+----+----+----+----+");
+             for (int j=0; j<gameBoard[i].length; j++)
+                 System.out.printf("| %s  ", gameBoard[i][j]);
+             System.out.println("|");
+         }
+         System.out.println("+----+----+----+----+----+");
+         System.out.println("\n\n");
+    }
     public List<Coordinates> getFreeCells(){
         List<Coordinates> freeCells = new ArrayList<>();
         for (int i = 0; i < gameBoard.length; i++) {

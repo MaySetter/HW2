f7a220d22bd71fdd7e568bb65bb95d9ee2c0b487
MaySetter <155473411+MaySetter@users.noreply.github.com>
2024-01-18 22:59:31 +0200

    Update Coordinates.java

--- src/XO/Coordinates.java
+++ src/XO/Coordinates.java
@@ -1,23 +1,24 @@
 package XO;
 /**
- * This class represent cell in the game board using rows and columns.
+ * This class represents a cell in the game board using rows and columns.
  * @author Nir Hazan 316009489 , May Seter 312123037
- *
  */
-public class Coordinates {
+
+public class Coordinates { 
 
     private int row;
     private int col;  // Define Variables.
+    
 /**
  * Constructor get row and column and create coordinate.
- * @param row
- * @param col
+ * @param row cell's row number
+ * @param col cell's column number
  */
     public Coordinates(int row, int col){ // Constructor
         this.row = row;
         this.col = col;
     }
 
-    public int getRow(){return row;} // Get function for variables.
+    public int getRow(){return row;} // Getters for variables.
     public int getCol(){return col;}
 }

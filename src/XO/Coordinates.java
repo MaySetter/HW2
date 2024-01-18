adcd3733e55779ce45dec6448c21275dd0e99baa
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-18 22:14:10 +0200

    Update Coordinates with notes.java

--- src/XO/Coordinates.java
+++ src/XO/Coordinates.java
@@ -1,15 +1,23 @@
 package XO;
-
+/**
+ * This class represent cell in the game board using rows and columns.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 public class Coordinates {
 
     private int row;
-    private int col;
-
-    public Coordinates(int row, int col){
+    private int col;  // Define Variables.
+/**
+ * Constructor get row and column and create coordinate.
+ * @param row
+ * @param col
+ */
+    public Coordinates(int row, int col){ // Constructor
         this.row = row;
         this.col = col;
     }
 
-    public int getRow(){return row;}
+    public int getRow(){return row;} // Get function for variables.
     public int getCol(){return col;}
 }

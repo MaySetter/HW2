eaa9c5718daf4a66c35c24150777feecf0554a89
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-18 22:16:21 +0200

    Update SelfGame with notes.java

--- src/XO/SelfGame.java
+++ src/XO/SelfGame.java
@@ -1,8 +1,13 @@
 package XO;
-
+/**
+ * This class represent user game in the game.
+ * build two player 2 self players.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 public class SelfGame extends Game{
-	SelfPlayer player1 =new SelfPlayer('X',this);
-	SelfPlayer player2=new SelfPlayer('O',this);
+	SelfPlayer player1 =new SelfPlayer(SIGNS.X.asChar,this);
+	SelfPlayer player2=new SelfPlayer(SIGNS.O.asChar,this);
 	public SelfGame() {
 		player1.start();
 		player2.start();

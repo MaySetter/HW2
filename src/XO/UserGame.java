295e51981ce7b1bdc41549d1f262b074327ae00e
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-18 22:17:06 +0200

    Update UserGame with notes.java

--- src/XO/UserGame.java
+++ src/XO/UserGame.java
@@ -1,8 +1,14 @@
 package XO;
+/**
+ * This class represent user game in the game.
+ * build two player 1 user 1 self player.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 
 public class UserGame extends Game {
-	SelfPlayer player1 =new SelfPlayer('X',this);
-	UserPlayer player2=new UserPlayer('O',this);
+	SelfPlayer player1 =new SelfPlayer(SIGNS.X.asChar,this);
+	UserPlayer player2=new UserPlayer(SIGNS.O.asChar,this);
 	public UserGame() {
 		player1.start();
 		player2.start();

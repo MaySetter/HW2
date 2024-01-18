71d7147a5f03cdb74d23989131639480a914302b
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-18 22:15:31 +0200

    Update Main with notes.java

--- src/XO/Main.java
+++ src/XO/Main.java
@@ -1,12 +1,17 @@
 package XO;
 import java.util.Scanner;
+/**
+ * This class represent main program in the game. user can choose mode.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 public class Main {
 	public static void main(String[] args) {
 		@SuppressWarnings("resource")
 		Scanner input = new Scanner(System.in);
 		int checker=0;
 		System.out.println("Choose :");
-		System.out.println("1.Self Game.");
+		System.out.println("1.Self Game.");          //Using input from user to decide which game mode should be run.
 		System.out.println("2.User Game.");
 		int mode=input.nextInt();
 		while (checker==0) {

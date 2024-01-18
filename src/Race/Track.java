3fed858f94ba30a51f5f14650d5a5162c3e303c6
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-01-18 22:13:38 +0200

    Update Track with notes.java

--- src/Race/Track.java
+++ src/Race/Track.java
@@ -1,11 +1,18 @@
 package Race;
-
+/**
+ * Class that represent track in the race.
+ * has how much racers finished.
+ * @author Nir Hazan 316009489 , May Seter 312123037
+ *
+ */
 public class Track {
-    private int finishedRacers;
+    private int finishedRacers; // Define Variables
+    /**
+     * Constructor of Track starting number of finihed racer to zero.
+     */
     Track(){
       this.finishedRacers=0;
     }
-    public int getFinishedRacers(){ return finishedRacers;}
+    public int getFinishedRacers(){ return finishedRacers;}  // get and set for finishedRacers variable.
     public void setFinishedRacers(){ finishedRacers++;}
 }
-

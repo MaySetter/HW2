ec9c9a0bfdbe778553cf5699cd4715761c82419d
MaySetter <155473411+MaySetter@users.noreply.github.com>
2024-01-25 21:10:22 +0200

    Final UserGame.java

--- src/XO/UserGame.java
+++ src/XO/UserGame.java
@@ -1,17 +1,24 @@
 package XO;
 /**
- * This class represent user game in the game.
+ * This class represent user game - one independent player (SelfPlayer)
+ * and one player controlled by user input (UserPlayer).
  * build two player 1 user 1 self player.
  * @author Nir Hazan 316009489 , May Seter 312123037
- *
  */
 
 public class UserGame extends Game {
-	SelfPlayer player1 =new SelfPlayer(SIGNS.X.asChar,this);
-	UserPlayer player2=new UserPlayer(SIGNS.O.asChar,this);
-	public UserGame() {
+	SelfPlayer player1;
+	UserPlayer player2;
+	public UserGame(SelfPlayer player1, UserPlayer player2) {	// constructor
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+	public void startGame() throws InterruptedException { 	// method sets the game for the players and starts their threads
+		player1.setGameForPlayer(this);
+		player2.setGameForPlayer(this);
 		player1.start();
 		player2.start();
+		player1.join();
+		player2.join();
 	}
-  
 }

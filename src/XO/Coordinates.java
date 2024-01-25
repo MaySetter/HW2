339ac28db85dedc72e30cd8567151eec9edf17c5
MaySetter <155473411+MaySetter@users.noreply.github.com>
2024-01-25 18:31:48 +0200

    Update Coordinates.java

--- src/XO/Coordinates.java
+++ src/XO/Coordinates.java
@@ -1,24 +1,11 @@
 package XO;
+
 /**
- * This class represents a cell in the game board using rows and columns.
  * @author Nir Hazan 316009489 , May Seter 312123037
+ * Record class Coordinates serves as "data carrier" for coordinates of game board cells.
+ * A record class declares a sequence of fields, and then the appropriate accessors,
+ * constructors, equals, hashCode, and toString methods are created automatically (Oracle).
+ * @param row cell's row number.
+ * @param col cell's column number.
  */
-
-public class Coordinates { 
-
-    private int row;
-    private int col;  // Define Variables.
-    
-/**
- * Constructor get row and column and create coordinate.
- * @param row cell's row number
- * @param col cell's column number
- */
-    public Coordinates(int row, int col){ // Constructor
-        this.row = row;
-        this.col = col;
-    }
-
-    public int getRow(){return row;} // Getters for variables.
-    public int getCol(){return col;}
-}
+public record Coordinates(int row, int col) {}

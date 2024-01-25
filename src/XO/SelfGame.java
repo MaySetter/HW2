c8fb8ef45a7a533291f399dc5e8d934b8208e6ba
MaySetter <155473411+MaySetter@users.noreply.github.com>
2024-01-25 19:02:23 +0200

    Update SelfGame.java

--- src/XO/SelfGame.java
+++ src/XO/SelfGame.java
@@ -1,16 +1,21 @@
 package XO;
 /**
- * This class represent user game in the game.
- * build two player 2 self players.
+ * This class represent self game - two independent players (SelfPlayer)
  * @author Nir Hazan 316009489 , May Seter 312123037
- *
  */
 public class SelfGame extends Game{
-	SelfPlayer player1 =new SelfPlayer(SIGNS.X.asChar,this);
-	SelfPlayer player2=new SelfPlayer(SIGNS.O.asChar,this);
-	public SelfGame() {
+	SelfPlayer player1;
+	SelfPlayer player2;
+
+	public SelfGame(SelfPlayer player1, SelfPlayer player2) { 	// constructor
+		this.player1 = player1;
+		this.player2 = player2;
+	}
+	
+	public void startGame(){ 	// method sets the game for the players and starts their threads
+		player1.setGameForPlayer(this);
+		player2.setGameForPlayer(this);
 		player1.start();
 		player2.start();
 	}
-  
 }
